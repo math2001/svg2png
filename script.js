13a0f065f56e7cea6112bdb43f1db145e634eecf
Mathieu PATUREL <australie.p@gmail.com>
2017-02-23 08:28:35 +1100

    auto add xmlns link when pasting code

--- script.js
+++ script.js
@@ -3,6 +3,13 @@
 const get = (selector, all=false) =>
     all ? document.querySelectorAll(selector) : document.querySelector(selector)
 
+function to_elements(string) {
+    // http://stackoverflow.com/a/494348/6164984
+    const div = document.createElement('div')
+    div.innerHTML = string
+    return [div.childNodes, div]
+}
+
 const canvas = document.createElement('canvas')
 const context = canvas.getContext('2d')
 const file_input = get('#file')
@@ -10,20 +17,21 @@ const resultImage = get('#result-image')
 const download = get('#download')
 const text_input = get('#text-input')
 
-const update_images = (pro) => {
+const update_images = () => {
+    const data = canvas.toDataURL()
+    resultImage.setAttribute('src', data)
+    download.download = 'image.png'
+    download.href = data
+}
+
+const calls = (pro) => {
     const image = new Image()
     image.onload = function () {
-        if (image) {
-            canvas.setAttribute('width', image.width + 'px')
-            canvas.setAttribute('height', image.height + 'px')
-        } else {
+        canvas.setAttribute('width', image.width + 'px')
+        canvas.setAttribute('height', image.height + 'px')
 
-        }
         context.drawImage(image, 0, 0)
-        const data = canvas.toDataURL()
-        resultImage.setAttribute('src', data)
-        download.download = 'image.png'
-        download.href = data
+        update_images()
     }
     if (typeof pro == 'string') {
         image.src = pro
@@ -44,12 +52,30 @@ function handleUpload(e) {
     }
 
     const fr = new FileReader()
-    fr.onloadend = update_images
+    fr.onloadend = calls
     fr.readAsDataURL(e.target.files[0])
 }
 
 function handleInput(e) {
-    update_images('data:image/svg+xml;base64,' + btoa(this.value))
+    // make the SVG a valid one
+    // * add the xmlns link
+    var [els, div] = to_elements(this.value)
+    var found = false
+    for (var i = 0; i < els.length; i++) {
+        if (els[i].nodeName != 'svg') {
+            continue
+        }
+        if (els[i].getAttribute('xmlns') == null) {
+            els[i].setAttribute('xmlns', "http://www.w3.org/2000/svg")
+        }
+        found = true
+        break
+    }
+    if (!found) {
+        alert("Didn't find any SVG tag in there.")
+    }
+
+    calls('data:image/svg+xml;base64,' + btoa(div.innerHTML))
 }
 
 file_input.addEventListener('change', handleUpload)
